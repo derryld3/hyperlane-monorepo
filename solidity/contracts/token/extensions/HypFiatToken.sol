58c4775e937a4cfaf10205fa86f3263cd7418162
Derryl <derryl@d3labs.io>
2025-08-20 10:02:50 +0700

    implement safe transfer

--- solidity/contracts/token/extensions/HypFiatToken.sol
+++ solidity/contracts/token/extensions/HypFiatToken.sol
@@ -3,9 +3,11 @@ pragma solidity >=0.8.0;
 
 import {IFiatToken} from "../interfaces/IFiatToken.sol";
 import {HypERC20Collateral} from "../HypERC20Collateral.sol";
+import {SafeERC20,IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 // see https://github.com/circlefin/stablecoin-evm/blob/master/doc/tokendesign.md#issuing-and-destroying-tokens
 contract HypFiatToken is HypERC20Collateral {
+    using SafeERC20 for IERC20;
 
     // Events
     event RouterFeeUpdated(uint32 indexed destination, uint256 fee);
@@ -55,7 +57,7 @@ contract HypFiatToken is HypERC20Collateral {
         if (isRouterFeeActive) {
             transferFee = routerFees[_destination];
             require(_amountOrId > transferFee, "Transfer amount must be greater than fee");
-            wrappedToken.transferFrom(msg.sender, address(this), transferFee);
+            IERC20(address(wrappedToken)).safeTransferFrom(msg.sender, address(this), transferFee);
             // Deduct fee from the amount being transferred
             amountAfterFee = _amountOrId - transferFee;
         }
